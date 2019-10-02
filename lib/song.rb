75192f326ea3f75a3dde125a0c7596e0e0bfba3a
Daniel Matney <danieljmatney@gmail.com>
2019-10-02 21:47:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count += 1
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.count
+    @@count
+  end
+end
